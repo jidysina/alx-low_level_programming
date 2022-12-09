02eedaed7d78e1eacd8dd6ef6892d9b7b5b0b705
jidysina <chukwuedojacinta@gmail.com>
2022-12-09 15:55:54 -0800

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entryy point of function
+ * Return: always (0) successful
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+			putchar('\n');
+	return (0);
+}

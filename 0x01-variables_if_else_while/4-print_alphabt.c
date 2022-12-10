171d0d0394beb216feb8f4f2e5522633bd3dc8f7
jidysina <chukwuedojacinta@gmail.com>
2022-12-09 17:01:38 -0800

    task 5

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always (0) successful
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+			putchar('\n');
+	return (0);
+}

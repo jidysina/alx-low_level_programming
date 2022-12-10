5058edf0ef3a1627eef8dd15d300d2be4eb74eff
jidysina <chukwuedojacinta@gmail.com>
2022-12-09 21:57:53 -0800

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always (0) sucessful
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+		putchar(ch);
+			putchar('\n');
+	return (0);
+}

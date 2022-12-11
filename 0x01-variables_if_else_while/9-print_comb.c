ee40676176b2f5f95b2a71c296c3f2296d988704
jidysina <chukwuedojacinta@gmail.com>
2022-12-11 14:56:42 -0800

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,8 +8,8 @@ int main(void)
 	int i = 0;
 
 	for (i = 0; i <= 9; i++)
-		putchar((i % 10) + '0');	
-	if (i == 9)	
+		putchar((i % 10) + '0');
+	if (i == 9)
 		putchar(',');
 			putchar(' ');
 			putchar('\n');

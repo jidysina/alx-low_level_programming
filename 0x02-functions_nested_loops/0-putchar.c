34f417c37243e7473fa65a45e1dea0d185bbbeef
jidysina <chukwuedojacinta@gmail.com>
2022-12-13 11:39:30 -0800

    task 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: (0) successful
+ */
+int main(void)
+{
+	char s[] = "_putchar";
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		putchar(s[i]);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
